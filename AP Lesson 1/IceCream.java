1951490a5a365da437899aeb680d0a29d5716bda
danrim99 <daniel.jin.rim@gmail.com>
2016-09-02 13:27:25 -0700

    Added files

---
+++ AP Lesson 1/IceCream.java
@@ -0,0 +1,30 @@
+public class IceCream
+{
+	public static void main (String[]args)
+	{
+		System.out.println("            .oo.");
+		System.out.println("          oGGGo");
+		System.out.println("         GGGGGG");
+		System.out.println("   .mMMMMGGGGG=");
+		System.out.println("  MMMMMMMMGGEEEE");
+		System.out.println(" MMMMMMMMNICKEEEE");
+		System.out.println(" MMMMMMMMMMEEEEEE");
+		System.out.println(" !MMMMMMMMOOEEEE");
+		System.out.println("   MMM!MMMOOO!=");
+		System.out.println("    MM!!!!!!!!!!!!!!!");
+		System.out.println("     MM!!!!!!!!!!!!!'");
+		System.out.println("     !M!!!!!!!!!!!!!");
+		System.out.println("      MM!!!!!!!!!'");
+		System.out.println("      MM!!!!!!!!");
+		System.out.println("     ! `!!!!!!!!!'");
+		System.out.println("      .  !!!!!!!!!");
+		System.out.println("        `!!!!!!!'");
+		System.out.println("          !!!!!!!");
+		System.out.println("          `!!!!!'");
+		System.out.println("           !!!!!");
+		System.out.println("           `!!!'");
+		System.out.println("             !!!");
+		System.out.println("              !'");
+		System.out.println("              !");
+	}
+}
