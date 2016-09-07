cfdde6cfcd3e68ed15b2d8d2f2d0b57b087f1200
danrim99 <daniel.jin.rim@gmail.com>
2016-09-07 13:53:31 -0700

    Added lesson 2

---
+++ APLesson_02/Lesson2.java
@@ -0,0 +1,11 @@
+public class Lesson2
+{
+	public static void main(String[]args)
+	{
+		int l = 8;
+		int w = 5;
+		int recArea = l* w;
+		
+		System.out.println("The area of you rectangle is " + recArea);
+	}
+}
