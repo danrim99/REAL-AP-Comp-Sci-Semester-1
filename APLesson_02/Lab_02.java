fcbe51943528d8376b7ca54b9f6c4ea8f760c676
danrim99 <daniel.jin.rim@gmail.com>
2016-09-07 14:30:53 -0700

    Lesson 2 Updated
    Added assignments for lesson 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,25 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int l = 19;
+		int w = 19;
+		int sqArea = l* w;
+		
+		System.out.println("Nineteen squared is " + sqArea);
+		
+		String name = "Daniel Rim, ";
+		String address = "5487 Sonoma Place, ";
+		String city = "San Diego, ";
+		String zip = "92130";
+		
+		System.out.println("For " + name + " the address is " + address + city + zip);
+		
+		int L = 2;
+		int W = 2;
+		int H = 2;
+		int SurfaceArea = L*W*6;
+		
+		System.out.println("The area of the solid is " + SurfaceArea);
+	}
+}
